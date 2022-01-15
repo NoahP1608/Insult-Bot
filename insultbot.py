11cd0cfcf39a87c0ba20df7ef20da76a2136ba15
NoahP1608 <58424626+NoahP1608@users.noreply.github.com>
2022-01-15 11:48:47 +0000

    Update insultbot.py

--- insultbot.py
+++ insultbot.py
@@ -1,7 +1,11 @@
 import discord
 import random
+import time
+import os
 
-TOKEN = [HIDDEN]
+
+def wait(time_amount):
+  time.sleep(time_amount)
 
 client = discord.Client()
 
@@ -11,37 +15,28 @@ insults = ['Fuck you', 'Get a life']
 @client.event
 
 async def on_ready():
+   #this is were the bot logs on
 
+  await client.change_presence(activity=discord.Game(name='On v1.04 !help'))
   print(f'Logged in')
-
-
-
-
-
-
-
-
-
+  
 
 
 @client.event
 
 async def on_message(message):
+  #this is were a mesage is sent
 
   username = str(message.author).split('#')[0]
   user_message = str(message.content)
   channel = str(message.channel.name)
 
-
-
-    
-
+  #now we have all the commands
   if user_message.lower() == '!hello':
     await message.channel.send(f'Hello!')
     print(f'!hello command used by {username}')
 
 
-  
   if user_message.lower() == '!insultme':
     choice = random.randint(1,2)
     if choice == 1:
@@ -49,7 +44,6 @@ async def on_message(message):
     elif choice == 2:
       await message.channel.send(f'Get a life {username}')
     print(f'!insultme command used by {username}')
-
 
 
   if user_message.lower() == '!flipacoin':
@@ -64,8 +58,44 @@ async def on_message(message):
   if user_message.lower() == '!pog':
     await message.channel.send(file=discord.File('pog.jpg'))
     print(f'!pog command used by {username}')
-    
+
+
+  if user_message.lower() == '!dicklength':
+
+    dicklength = random.randint(1,10)
+    await message.channel.send(f'{username}s dick length is...{dicklength} inches')
+    print(f'!dicklength command used by {username}')
+
+
+  if user_message.lower() == '!randomnumber':
+    number = random.randint(1,100)
+    await message.channel.send(f'Your random number is {number}')
+    print(f'!randomnumber command used by {username}')
+
+
+  if user_message.lower() == '!help':
+    await message.channel.send('!hello !insultme !pog !dicklength !randomnumber !ball')
+    print(f'!help command used by {username}')
+
+
+  if '!ball' in user_message:
+    yn = random.randint(1,2)
+    if yn == 1:
+      await message.channel.send('Yes')
+    else:
+      await message.channel.send('No')
+    print(f'!ball command used by {username}')
+
+
+  if user_message.lower() == '!pope':
+    await message.channel.send(file=discord.File('spinning-priest-priest.gif'))
+    print(f'!pope command used by {username}')
 
   
 
-client.run(TOKEN)
+#all repl.its are public so i needed to hide the token
+my_secret = os.environ['token']
+client.run(my_secret)
+
+#gtg cya tmmr
+
